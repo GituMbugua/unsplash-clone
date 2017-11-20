7307e5eef20b2100a5f351cb172593d55741f935
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-21 00:31:47 +0300

    add view function for  images

--- unsplash/images/views.py
+++ unsplash/images/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Image, tags
 
 def home(request):
-    return render(request, 'home.html')
+    '''
+    display all images on home page
+    '''
+    images = Image.get_images
+    return render(request, 'home.html', {"images":images})
+
+def tags(request):
+    '''
+    display all tags
+    '''
+    all_tags = tags.get_tags
+    print('all_tags')
+    return render(request, 'nav_buttons.html', {"all_tags":all_tags})
