5e25a5f7d0512956d8e1034d55e3d42933e87cf4
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-21 00:30:43 +0300

    register admin

--- unsplash/images/admin.py
+++ unsplash/images/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Image, tags
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(tags)
