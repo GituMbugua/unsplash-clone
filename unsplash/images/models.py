f345af715deee5dc3241468180436236e2ee95d1
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-21 00:30:55 +0300

    add models

--- unsplash/images/models.py
+++ unsplash/images/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_user(self):
+        self.save()
+
+class tags(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_tags(cls):
+        tags = cls.objects.all()
+        return tags
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to = 'photos/')
+    author = models.ForeignKey(User)
+    tags = models.ManyToManyField(tags)
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
