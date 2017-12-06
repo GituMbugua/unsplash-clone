d64c6e49a443e5e9f99257e3abd9041132308a7a
Gitu Mbugua <gmbugua38@gmail.com>
2017-12-06 17:03:51 +0300

    add test for searching by tags

--- unsplash/images/tests.py
+++ unsplash/images/tests.py
@@ -20,7 +20,7 @@ class ImageTestClass(TestCase):
     def setUp(self):
         # create new user instance and save
         self.gitu = User(first_name = 'Gitu', last_name = 'Mbugua', email = 'gitu@testing.com', phone_number = '0700123456')
-        self.git.save_user()
+        self.gitu.save_user()
 
         # create new tag and save
         self.new_tag = tags(name = 'testing')
@@ -37,6 +37,27 @@ class ImageTestClass(TestCase):
         tags.objects.all().delete()
         Image.objects.all().delete()
 
-    def test_get_images(self):
-        images = Image.get_images()
-        self.assertTrue(len(get_images) > 0)
+    # def test_get_images(self):
+    #     images = Image.get_images()
+    #     self.assertTrue(len(get_images) > 0)
+
+class tagsTestClass(TestCase):
+    '''
+    Test case for tags class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up a tags instance before each test
+        '''
+        # Create a tags instance
+        self.new_tag = tags(name='Python')
+
+    def test_search_by_tags(self):
+        '''
+        Test case to check if tags containg a search term is gotten from the database 
+        '''
+        self.new_tag.save_tags()
+        found_tags = tags.search_by_tags('Python')
+        self.assertTrue( len(found_tags) == 1)
